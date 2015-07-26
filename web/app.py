0c312f210476453b2d0a2943399ae396850e4c15
Eben Olson <eben.olson@gmail.com>
2015-07-26 16:57:26 -0400

    update

--- web/app.py
+++ web/app.py
@@ -5,13 +5,13 @@ from flask import render_template
 
 app = Flask(__name__)
 
-data = pd.load('../notebooks/data2.df').sort('upvotes', ascending=False)
+data = pd.load('../notebooks/data3.df').sort('upvotes', ascending=False)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', data=data[:100])
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
